be8b5588bf7ed0e79f49ff56417b4f124c96a678
Gustavo F Costa <gufercosta@gmail.com>
2022-11-01 11:08:02 -0300

    prisma generate user

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, UserModule],
   controllers: [],
   providers: [],
 })
